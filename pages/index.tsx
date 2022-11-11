ca3abb04260623ff1d1b68880793a992ceaa3ffe
duwonKim <dev.dothe1@gmail.com>
2022-11-11 18:09:00 +0900

    20221111-1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,121 +1,81 @@
-export default function Home() {
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
   return (
-    <>
-      <div className="bg-slate-400 py-20 px-20 flex flex-col space-y-5 min-h-screen ">
-        <div className="bg-white p-6 rounded-3xl shadow-xl">
-          <span className="font-semibold text-3xl">Select Item</span>
-          <ul>
-            {[1, 2, 3, 4, 5].map((i) => (
-              <div
-                key={i}
-                className="flex justify-between my-2 first:bg-blue-50 last:bg-blue-50"
+    <div className="px-4">
+      {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+        <div
+          key={i}
+          className="flex justify-between py-2 border-t border-gray-200"
+        >
+          <div className="flex">
+            <div className="w-20 h-20 bg-gray-400 rounded-md mr-4" />
+            <div className="flex flex-col space-y-1 justify-center">
+              <h3 className="text-sm">New iPhone 14</h3>
+              <span className="text-xs text-gray-500">Black</span>
+              <span className="text-sm font-medium">$95</span>
+            </div>
+          </div>
+
+          <div className="flex justify-end items-end space-x-2">
+            <div className="flex items-center text-xs text-gray-600">
+              <svg
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                <span className="text-gray-500">Grey chair</span>
-                <span className="font-semibold">$39</span>
-              </div>
-            ))}
-            {/* 
-            <div className="flex justify-between my-2">
-              <span className="text-gray-500">Grey chair</span>
-              <span className="font-semibold">$39</span>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                ></path>
+              </svg>
+              <span>1</span>
             </div>
 
-            <div className="flex justify-between my-2">
-              <span className="text-gray-500">Tooly table</span>
-              <span className="font-semibold">$19</span>
-            </div> */}
-          </ul>
-
-          <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
-            <span>Total</span>
-            <span className="font-semibold">$99</span>
-          </div>
-          <button className="mt-5 hover:bg-teal-500 active:bg-blue-400 focus:text-red-500 bg-blue-400 text-white p-3 text-center rounded-xl w-1/2 mx-auto">
-            checkout
-          </button>
-        </div>
-
-        <div className="bg-white rounded-2xl shadow-xl overflow-hidden ">
-          <div className="bg-rose-300 p-6 pb-14">
-            <span className="text-white text-2xl">Profile</span>
-          </div>
-          <div className="rounded-2xl bg-white relative -top-5 p-6 group transition">
-            <div className="flex items-end justify-between relative -top-16">
-              <div className="flex flex-col items-center">
-                <span className="text-sm text-gray-400">Orders</span>
-                <span className="font-medium">340</span>
-              </div>
-              <div className="h-24 w-24 bg-red-400 rounded-full flex justify-center items-center text-4xl group-hover:bg-purple-400">
-                👩‍🦰
-              </div>
-              <div className="flex flex-col items-center">
-                <span className="text-sm text-gray-400">Spent</span>
-                <span className="font-medium">$2,310</span>
-              </div>
-            </div>
-            <div className="relative -mt-16 flex flex-col items-center">
-              <span className="text-lg font-medium">TONY</span>
-              <span className="text-sm text-gray-500">Sanfrancisco, USA</span>
+            <div className="flex items-center text-xs text-gray-600">
+              <svg
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                ></path>
+              </svg>
+              <span>1</span>
             </div>
           </div>
         </div>
+      ))}
 
-        <div className="bg-white p-10 rounded-2xl shadow-xl">
-          <div className="flex justify-between items-center mb-5">
-            <span>&larr;</span>
-            <div className="space-x-4">
-              <span>⭐ 4.9</span>
-              <span className="shadow-xl p-2 rounded-md">❤</span>
-            </div>
-          </div>
-          <div className=" max-w-full mb-5 flex justify-center items-center">
-            <div className="bg-zinc-400 h-72 w-72"></div>
-          </div>
-          <div className="flex flex-col">
-            <span className="font-medium text-xl">Swoon Lounge</span>
-            <span className="text-xs text-gray-500">Chair</span>
-            <div className="mt-2 mb-5 flex justify-between items-center">
-              <div className="space-x-3">
-                <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-offset-2 ring-yellow-500 transition"></button>
-                <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-offset-2 ring-indigo-500 transition"></button>
-                <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-offset-2 ring-teal-500 transition"></button>
-              </div>
-              <div className="flex items-center space-x-5">
-                <button className="p-2 bg-blue-200 flex rounded-lg justify-center items-center aspect-square w-10 font-medium text-xl text-gray-500">
-                  -
-                </button>
-                <span>1</span>
-                <button className="p-2 bg-blue-200 flex rounded-lg justify-center items-center aspect-square w-10 font-medium text-xl text-gray-500">
-                  +
-                </button>
-              </div>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-medium text-2xl">$460</span>
-              <button className="bg-blue-500 text-center text-white rounded-lg p-4">
-                Add to cart
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="bg-white p-10 rounded-2xl shadow-xl">
-          <form className="flex flex-col space-y-2 bg-blue-400 p-3 focus-within:bg-blue-100">
-            <input
-              type="text"
-              required
-              placeholder="userNm"
-              className="required:border-2 border-red-600"
-            />
-            <input type="password" required placeholder="userPw" />
-            <input
-              type="submit"
-              required
-              value="login 하기"
-              className="border-2"
-            />
-          </form>
-        </div>
-      </div>
-    </>
+      <button className="fixed bg-orange-500 hover:bg-orange-600 cursor-pointer transition-colors text-white w-10 h-10 bottom-10 right-10 flex justify-center items-center rounded-full shadow-md">
+        <svg
+          className="h-6 w-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          aria-hidden="true"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+          />
+        </svg>
+      </button>
+    </div>
   );
-}
+};
+
+export default Home;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [require("@tailwindcss/forms")],
 };
